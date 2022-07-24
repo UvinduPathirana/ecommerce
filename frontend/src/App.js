282dd7cdb990b234d1fba3d400228452527be0ae
Uvindu-bit <upathirana112@gmail.com>
2022-07-24 14:05:26 +0530

    implemented react rounter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,27 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container }  from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
     <div>
-      <Header />
+    <Header />
       <main className="py-3">
         <Container>
-        <HomeScreen />
+        <Routes>
+          <Route exact path='/' element={< HomeScreen />}></Route>
+          <Route path='/product/:id' element={< ProductScreen />}></Route>
+        </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+        </div>
   );
 }
 
