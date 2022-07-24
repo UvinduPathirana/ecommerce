c66385ec29749cfa991caf6691d7202a4ca39693
Uvindu-bit <upathirana112@gmail.com>
2022-07-24 14:13:20 +0530

    new routed added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,22 +5,25 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function Header() {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container fluid>
-            <Navbar.Brand href="#">Proshop</Navbar.Brand>
+            <LinkContainer to='/'>
+                <Navbar.Brand>Proshop</Navbar.Brand>
+            </LinkContainer>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
-            <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
-            >
-                <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-                <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
+            <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+                <LinkContainer to='/cart'>
+                    <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/login'>
+                    <Nav.Link><i className='fas fa-user'></i>Login</Nav.Link>
+                </LinkContainer>
             </Nav>
             <Form className="d-flex">
                 <Form.Control
